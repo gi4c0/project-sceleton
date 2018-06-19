2816c5f030ecffada149c074f394bad3f508e270
gtament <denis.braslavskiy@do-it.co>
2018-06-19 15:48:05 +0300

    add default eslint and router

--- src/startup/middleware.js
+++ src/startup/middleware.js
@@ -1,8 +1,11 @@
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
+const router = require('../routes/indexRoute.js')
+
 module.exports = app => {
   app.use(morgan('dev'))
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
+  app.use(router)
 }
