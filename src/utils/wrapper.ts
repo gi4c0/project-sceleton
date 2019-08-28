831cef3608a161e1f525abadf5c340b229aac2fd
alexpan <p.aleksey@do-it.co>
2019-08-28 12:42:37 +0300

    Update wrapper

--- src/utils/wrapper.ts
+++ src/utils/wrapper.ts
@@ -4,11 +4,20 @@ interface IUser {
   user: {
     id: number
     email: string
+    chosenCompanyId: number
+
+    lastUsedAccountId: number
   }
 }
 
-type IFn = (req: Request & IUser, res: Response, next: NextFunction) => Promise<any>
+interface ITypedRequest <Body, Query, Params> extends Request {
+  body: Body
+  query: Query
+  // params: Params
+}
 
-export const wrapper = (fn: IFn) => (req, res, next) => {
+type Fn<B, Q, P> = (req: ITypedRequest<B, Q, P> & IUser, res: Response, next: NextFunction) => Promise<any>
+
+export const wrapper = <Body = any, Query = any, Params = any>(fn: Fn<Body, Query, Params>) => (req, res, next) => {
   fn(req, res, next).catch(next)
 }
